fefe406c320e3256be0eb6cd2293a067a2ba12d7
1320503154 <1320503154@qq.com>
2023-05-12 16:17:30 +0800

    Update server.js
    大致添加了发送消息得框架,还是得return这个函数,return!!!!

--- src/server/server.js
+++ src/server/server.js
@@ -2,3 +2,17 @@ import axios from "axios";
 axios.defaults.timeout = 3000; //3秒的等待时间,3秒到了不再等待response
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "ws://keixe.space/chat/";
+const getUserList = axios.create();
+export function PostMessage(message) {
+	axios.post({
+		url: "msg",
+		data: {
+			type: "msg",
+			sender: 5,
+			msg: message,
+		},
+	});
+}
+export function getOnlineUserList() {
+	getUserList.get("https://kexie.space/chat/data/onlinelist");
+}
